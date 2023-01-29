12c4981c387fd72c3fc5041203e06591673f4962
Benjamin Morel <benjamin.morel@gmail.com>
2023-01-29 14:49:48 +0100

    Add TypeParser

---
+++ src/Reflection/TypeParser.php
@@ -0,0 +1,198 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\JsonMapper\Reflection;
+
+use Brick\JsonMapper\JsonMapperException;
+
+/**
+ * Parses the given string and returns a nested representation of the types.
+ *
+ * Examples:
+ *
+ * - 'string' => ['string']
+ * - 'string|null' => ['string', 'null']
+ * - 'int[]|null' => [['int'], 'null']
+ * - (string|int)[] => [['string', 'int']]
+ * - (string|int)[]|null => [['string', 'int'], 'null']
+ *
+ * Disallowed:
+ *
+ * - nullable types: '?string'
+ * - generics style: 'array<string>'
+ * - intersection types: 'A&B'
+ */
+final class TypeParser
+{
+    /**
+     * @var TypeToken[]
+     *
+     * @psalm-var list<TypeToken>
+     */
+    private array $tokens = [];
+
+    private int $pointer = 0;
+
+    public function __construct(
+        private string $type,
+    ) {
+    }
+
+    /**
+     * @throws JsonMapperException
+     */
+    public function parse(): array
+    {
+        $this->tokens = TypeTokenizer::tokenize($this->type);
+        $this->pointer = 0;
+
+        $result = $this->parseUnion(false);
+
+        $nextToken = $this->nextToken();
+
+        if ($nextToken !== null) {
+            $this->failExpectation(['end of string', '|', '[]'], $nextToken);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @throws JsonMapperException
+     */
+    private function parseUnion(bool $nested): array
+    {
+        $values = [];
+
+        for (;;) {
+            $value = $this->parseValue();
+
+            if ($this->isNextToken('[]')) {
+                if (is_array($value)) {
+                    $this->advance();
+                }
+
+                while ($this->isNextToken('[]')) {
+                    $this->advance();
+                    $value = [$value];
+                }
+
+                $values[] = $value;
+            } elseif (is_array($value)) {
+                // unnecessary parentheses, merge into current values
+                $values = array_merge($values, $value);
+            } else {
+                $values[] = $value;
+            }
+
+            $peekToken = $this->peekToken();
+
+            if ($peekToken === null || $peekToken->value === ')') {
+                break;
+            }
+
+            if ($peekToken->value === '|') {
+                $this->advance();
+                continue;
+            }
+
+            $this->failExpectation(
+                array_merge(['|', '[]'], $nested ? [')'] : []),
+                $peekToken,
+            );
+        }
+
+        return $values;
+    }
+
+    /**
+     * @throws JsonMapperException
+     */
+    private function parseValue(): string|array
+    {
+        $nextToken = $this->nextToken();
+
+        if ($nextToken !== null) {
+            if ($nextToken->isNamedType) {
+                return $nextToken->value;
+            }
+
+            if ($nextToken->value === '(') {
+                $value = $this->parseUnion(true);
+
+                $nextToken = $this->nextToken();
+
+                if ($nextToken === null || $nextToken->value !== ')') {
+                    $this->failExpectation([')', '|', '[]'], $nextToken);
+                }
+
+                return $value;
+            }
+        }
+
+        $this->failExpectation(['named type', '('], $nextToken);
+    }
+
+    /**
+     * Retrieves the next token, and advances the pointer.
+     */
+    private function nextToken(): ?TypeToken
+    {
+        if ($this->pointer === count($this->tokens)) {
+            return null;
+        }
+
+        return $this->tokens[$this->pointer++];
+    }
+
+    /**
+     * Peeks at the next token, without advancing the pointer.
+     */
+    private function peekToken(): ?TypeToken
+    {
+        if ($this->pointer === count($this->tokens)) {
+            return null;
+        }
+
+        return $this->tokens[$this->pointer];
+    }
+
+    /**
+     * Advances the pointer.
+     */
+    private function advance(): void
+    {
+        $this->pointer++;
+    }
+
+    /**
+     * Checks if the next token, if any, matches the given value.
+     * Does not advance the pointer.
+     */
+    private function isNextToken(string $value): bool
+    {
+        $nextToken = $this->peekToken();
+
+        return $nextToken !== null && $nextToken->value === $value;
+    }
+
+    /**
+     * @param string[] $expected
+     *
+     * @throws JsonMapperException
+     */
+    private function failExpectation(array $expected, ?TypeToken $actual): never
+    {
+        $expected = implode(' or ' , array_map(
+            fn (string $value) => in_array($value, ['(', ')', '|', '[]'], true) ? "\"$value\"" : $value,
+            $expected,
+        ));
+
+        $actual = ($actual === null)
+            ? 'end of string'
+            : sprintf('"%s" at offset %d', $actual->value, $actual->offset);
+
+        throw new JsonMapperException(sprintf('Expected %s, found %s.', $expected, $actual));
+    }
+}
---
+++ tests/Reflection/TypeParserTest.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\JsonMapper\Tests\Reflection;
+
+use Brick\JsonMapper\JsonMapperException;
+use Brick\JsonMapper\Reflection\TypeParser;
+use PHPUnit\Framework\TestCase;
+
+final class TypeParserTest extends TestCase
+{
+    /**
+     * @dataProvider providerParse
+     */
+    public function testParse(string $type, array $expected): void
+    {
+        $typeParser = new TypeParser($type);
+        self::assertSame($expected, $typeParser->parse());
+    }
+
+    public function providerParse(): array
+    {
+        return [
+            ['string', ['string']],
+            ['string|null', ['string', 'null']],
+            ['(string|null)[]', [['string', 'null']]],
+            ['string|int[]', ['string', ['int']]],
+            ['string|(int|null)[]', ['string', ['int', 'null']]],
+            ['A|B|C', ['A', 'B', 'C']],
+            ['A|B|C[]|D[]', ['A', 'B', ['C'], ['D']]],
+            ['A|B|(C|D[])[]|E[]', ['A', 'B', ['C', ['D']], ['E']]],
+            ['A|B|(C|D[])[][]|E[]', ['A', 'B', [['C', ['D']]], ['E']]],
+            ['(A)|B', ['A', 'B']],
+            ['int', ['int']],
+            ['int[]', [['int']]],
+            ['int[][]', [[['int']]]],
+            ['(int)', ['int']],
+            ['((int))', ['int']],
+            ['(((int)))[]', [['int']]],
+            ['(int)[]', [['int']]],
+            ['(int)[][][]', [[[['int']]]]],
+            ['((int)[])[]', [[['int']]]],
+            ['((int|false)[])[]', [[['int', 'false']]]],
+            ['((int|false)[]|null)[]', [[['int', 'false'], 'null']]],
+            ['((int|false)[]|null)[]|true', [[['int', 'false'], 'null'], 'true']],
+            ['string|(int|float)[]', ['string', ['int', 'float']]],
+            ['int[]|(string|A|B|(int|float)[])[]|null', [['int'], ['string', 'A', 'B', ['int', 'float']], 'null']],
+            ['(int)[]|(string|int)[]|float|string', [['int'], ['string', 'int'], 'float', 'string']],
+        ];
+    }
+
+    /**
+     * @dataProvider providerParseInvalidType
+     */
+    public function testParseInvalidType(string $type, string $exceptionMessage): void
+    {
+        $typeParser = new TypeParser($type);
+
+        self::expectException(JsonMapperException::class);
+        self::expectExceptionMessage($exceptionMessage);
+
+        $typeParser->parse();
+    }
+
+    public function providerParseInvalidType(): array
+    {
+        return [
+            ['', 'Expected named type or "(", found end of string.'],
+            [' ', 'Expected named type or "(", found end of string.'],
+            ['A | ', 'Expected named type or "(", found end of string.'],
+            ['?string', 'Unexpected "?" at offset 0.'],
+            ['array<string>', 'Unexpected "<" at offset 5.'],
+            ['|', 'Expected named type or "(", found "|" at offset 0.'],
+            ['[]', 'Expected named type or "(", found "[]" at offset 0.'],
+            ['()', 'Expected named type or "(", found ")" at offset 1.'],
+            ['()[]', 'Expected named type or "(", found ")" at offset 1.'],
+            ['(|)[]', 'Expected named type or "(", found "|" at offset 1.'],
+            ['|[]', 'Expected named type or "(", found "|" at offset 0.'],
+            [')', 'Expected named type or "(", found ")" at offset 0.'],
+            ['(', 'Expected named type or "(", found end of string.'],
+            [')[]', 'Expected named type or "(", found ")" at offset 0.'],
+            ['(A)[', 'Char "[" is not followed by "]" at offset 3.'],
+            ['(A)]', 'Char "]" is not preceded by "[" at offset 3.'],
+            ['A A', 'Expected "|" or "[]", found "A" at offset 2.'],
+            ['(A A)', 'Expected "|" or "[]" or ")", found "A" at offset 3.'],
+            ['((A)A)', 'Expected "|" or "[]" or ")", found "A" at offset 4.'],
+            ['(A)(A)', 'Expected "|" or "[]", found "(" at offset 3.'],
+            ['((A)(A))', 'Expected "|" or "[]" or ")", found "(" at offset 4.'],
+            ['A|', 'Expected named type or "(", found end of string.'],
+            ['|B', 'Expected named type or "(", found "|" at offset 0.'],
+            ['(A|)[]', 'Expected named type or "(", found ")" at offset 3.'],
+            ['(|B)[]', 'Expected named type or "(", found "|" at offset 1.'],
+            ['A||B', 'Expected named type or "(", found "|" at offset 2.'],
+            ['(A[]', 'Expected ")" or "|" or "[]", found end of string.'],
+            ['B)[]', 'Expected end of string or "|" or "[]", found ")" at offset 1.'],
+            ['(A', 'Expected ")" or "|" or "[]", found end of string.'],
+            ['B)', 'Expected end of string or "|" or "[]", found ")" at offset 1.'],
+            ['A&B', 'Unexpected "&" at offset 1.'],
+            ['A*B', 'Unexpected "*" at offset 1.'],
+            ['A/B', 'Unexpected "/" at offset 1.'],
+            ['A|B=|C', 'Unexpected "=" at offset 3.'],
+            ['A|B|', 'Expected named type or "(", found end of string.'],
+            ['((A)[]', 'Expected ")" or "|" or "[]", found end of string.'],
+            ['(A))[]', 'Expected end of string or "|" or "[]", found ")" at offset 3.'],
+        ];
+    }
+}
